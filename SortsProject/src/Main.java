a8cab098aeee79c6e835bc4e10acb05e00447fdd
gruntkrusher <gruntkrusher@gmail.com>
2017-02-09 12:02:53 -0500

    Add files via upload

--- SortsProject/src/Main.java
+++ SortsProject/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 public class Main {
 	
 	public static void main(String[] args) {
+		double nanoToSec = 1000000000;
 		long[][] plotData = new long[1][3];
 		int[] unsortedData;
 		SortTester sT = new SortTester();
@@ -13,6 +14,8 @@ public class Main {
 		
 		for(int i = 0; i < plotData.length; i++) {
 			System.out.println("bubble: " + plotData[i][0] + " selection: " + plotData[i][1] + " insertion: " + plotData[i][2]);
+			System.out.println("bubble: " + plotData[i][0]/nanoToSec + " selection: " + plotData[i][1]/nanoToSec + " insertion: " + plotData[i][2]/nanoToSec);
+			System.out.println();
 		}
 	}
 	
