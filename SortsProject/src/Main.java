b87239004a3c6b69bc4f774677f6f7f44c9998b0
gruntkrusher <gruntkrusher@gmail.com>
2017-02-26 17:53:27 -0500

    Sorts Update

--- SortsProject/src/Main.java
+++ SortsProject/src/Main.java
@@ -1,18 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 
 public class Main {
 	
 	public static void main(String[] args) {
+		float NanoToSec = 1000000000;
 		long[][] plotData = new long[1][3];
 		int[] unsortedData;
 		SortTester sT = new SortTester();
 		
-		unsortedData = best(1000000);
+		unsortedData = random(100000);
 		plotData[0] = sT.test(unsortedData);
 		
 		for(int i = 0; i < plotData.length; i++) {
 			System.out.println("bubble: " + plotData[i][0] + " selection: " + plotData[i][1] + " insertion: " + plotData[i][2]);
+			System.out.println("bubble: " + plotData[i][0]/NanoToSec + " selection: " + plotData[i][1]/NanoToSec + " insertion: " + plotData[i][2]/NanoToSec);
 		}
 	}
 	
@@ -45,10 +49,24 @@ public class Main {
 		return a;
 	}
 	
+	static int[] random(final int size) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < size; i++) {
+			list.add(i+1);
+		}
+		Collections.shuffle(list);
+		
+		int[] a = new int[size];
+		for(int i = 0; i < size; i++) {
+			a[i] = list.get(i);
+		}
+		
+		return a;
+	}
+	
 	static void swap(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
-	}
-	
+	}	
 }
